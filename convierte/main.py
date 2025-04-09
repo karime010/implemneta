71d752132820193fe509267586b41510a0e5f399
= <=>
2025-04-09 09:01:37 -0600

    convertir

--- convierte/main.py
+++ convierte/main.py
@@ -1,13 +1,18 @@
 import tkinter as tk
+from tkinter import messagebox
 ventana = tk.Tk()
 ventana.title("conversion")
 ventana.geometry("400x500")
-boton = tk.Button (ventana,text= "Binario")
+boton = tk.Button (ventana,text= "Binario",command=toBin)
 boton.pack()
 
-import tkinter as tk
-from tkinter import messagebox
 resultado =""
+
+txt=tk.Entry(ventana)
+txt.pack()
+
+Ibl_resultado=tk.Label(ventana)
+Ibl_resultado.pack()
 
 def toBin():
     try:
